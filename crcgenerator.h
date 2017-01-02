ba6650899175de0f6099eec507de55b14574c3e9
Thomas Harte <thomas.harte@gmail.com>
2017-01-02 13:25:18 -0500

    Added some headerdoc comments.

--- crcgenerator.h
+++ crcgenerator.h
@@ -11,13 +11,46 @@
 
 #include <stdint.h>
 
+/*!
+	Opaquely represents a CRC generator. Use @c crc_create to construct and @c crc_destroy to dispose.
+*/
 typedef void CRCGenerator;
 
+/*!
+	Creates a new 16-bit CRC generator. It will be seeded with the value 0xffff.
+
+	@param polynomial this CRC generator's polynomial.
+	@returns a CRC generator with a heap footprint that you own.
+	@sa @c crc_destroy to release resources associated with the CRC generator.
+*/
 CRCGenerator *crc_create(uint16_t polynomial);
+
+/*!
+	Releases all resources associated with @c generator.
+
+	@param generator the generator to destroy.
+*/
 void crc_destroy(CRCGenerator *generator);
 
-void crc_reset_to_value(CRCGenerator *, uint16_t value);
-void crc_add_byte(CRCGenerator *, uint8_t byte);
-uint16_t crc_get_value(CRCGenerator *);
+/*!
+	Sets the current value for @c generator.
+
+	@param generator the generator to set a value for.
+	@param value the value to set.
+*/
+void crc_reset_to_value(CRCGenerator *generator, uint16_t value);
+
+/*!
+	Adds a byte to the CRC generator.
+
+	@param generator the generator to add the byte to.
+	@param byte the byte to add to the generator.
+*/
+void crc_add_byte(CRCGenerator *generator, uint8_t byte);
+
+/*!
+	@returns the current value of @c generator.
+*/
+uint16_t crc_get_value(CRCGenerator *generator);
 
 #endif /* crcgenerator_h */
